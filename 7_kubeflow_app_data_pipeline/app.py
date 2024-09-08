a51d535ad9655aec9d8a485ad3cf9ec22182cb86
Nagesh Rathod <111345802+nageshredhat@users.noreply.github.com>
2024-09-08 20:40:20 +0000

    host added

--- 7_kubeflow_app_data_pipeline/app.py
+++ 7_kubeflow_app_data_pipeline/app.py
@@ -87,6 +87,6 @@ def index():
 # if __name__ == "__main__":
 #     app.run(debug=False, port=5050, use_reloader=False, use_debugger=False)
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
 
